ca103f4227d6528baf23d2afadf0d9f4e8b91402
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2015-04-22 21:34:16 -0700

    Add table name to migration

--- db/migrate/20150420023559_add_upload_to_customization_value.rb
+++ db/migrate/20150420023559_add_upload_to_customization_value.rb
@@ -1,8 +1,8 @@
 class AddUploadToCustomizationValue < ActiveRecord::Migration
   def change
-    add_column :upload_file_name, :string
-    add_column :upload_content_type, :string
-    add_column :upload_file_size, :integer
-    add_column :upload_updated_at, :datetime
+    add_column :shoppe_customization_values, :upload_file_name, :string
+    add_column :shoppe_customization_values, :upload_content_type, :string
+    add_column :shoppe_customization_values, :upload_file_size, :integer
+    add_column :shoppe_customization_values, :upload_updated_at, :datetime
   end
 end
